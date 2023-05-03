b1603dffae13da1d1d73dec72943f76493f22c25
constantinostrada <constantinostrada@gmail.com>
2023-05-03 12:50:12 -0300

    Navbar Component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 
-import useCurrentUser from '@/hooks/useCurrentUser';
+import NavBar from '@/components/NavBar';
 import { NextPageContext } from 'next'
-import { getSession,signOut } from 'next-auth/react'
+import { getSession } from 'next-auth/react'
 
 
 export async function getServerSideProps(context:NextPageContext) {
@@ -21,15 +21,11 @@ export async function getServerSideProps(context:NextPageContext) {
 
 }
 export default function Home() {
-const {data:user} = useCurrentUser();
   
 
   return (
     <>
-     
-     <h1 className="text-green-500 text-4xl " >Netflix clone</h1>
-     <p className='text-white' >Logged in as: {user?.email}</p>
-    <button className='h-10 w-full bg-white' onClick={()=>signOut()} >Logout!</button>
+     <NavBar></NavBar>
     </>
   )
 }
